c807ed49efc6782dd7484a618b6ebaa05c80e413
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2025-11-05 16:56:35 -0500

    fixed port

--- DBConnection.js
+++ DBConnection.js
@@ -14,6 +14,7 @@ dotenv.config()
 
 export const db = mysql.createConnection({
     host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE
