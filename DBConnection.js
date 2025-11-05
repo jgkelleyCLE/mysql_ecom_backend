52b15a9ef5e3a006fb30b875462bdad57e4d86cf
Jack <74841896+jgkelleyCLE@users.noreply.github.com>
2025-11-05 16:35:21 -0500

    database connection

--- DBConnection.js
+++ DBConnection.js
@@ -3,9 +3,18 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+
+//LOCAL
+// export const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: process.env.DB_PASS,
+//     database: 'safariProducts'
+// })
+
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: process.env.DB_PASS,
-    database: 'safariProducts'
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 })
